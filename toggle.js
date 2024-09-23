16ec222b06cb8ced0d9745a307441e00c54d1612
Andreea Megherlich <andreea.meghe@gmail.com>
2024-09-23 11:32:22 +0300

    show current layout on header

--- toggle.js
+++ toggle.js
@@ -2,12 +2,15 @@ function toggleLayout() {
     const flexboxStylesheet = document.getElementById('flex_style');
     const gridStylesheet = document.getElementById('grid_style');
     const toggleButton = document.querySelector('.toggle-btn');
+    const layoutTitle = document.querySelector('.layout-title');
 
     if (flexboxStylesheet.disabled) {
         flexboxStylesheet.disabled = false;
         gridStylesheet.disabled = true;
+        layoutTitle.textContent = "Flexbox Layout page";
     } else {
         flexboxStylesheet.disabled = true;
         gridStylesheet.disabled = false;
+        layoutTitle.textContent = "Grid Layout page";
     }
 }
